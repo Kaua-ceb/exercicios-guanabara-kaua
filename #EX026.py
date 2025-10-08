ca6eef65367a8b1e4deb050ed7eabcd734559634
Kaua-ceb <Kauapereirarangel1207@gmail.com>
2025-10-07 22:28:33 -0300

    Delete #EX026.py

--- #EX026.py
+++
@@ -1,5 +0,0 @@
-#EX026
-frs = str(input("digite uma frase: ")).strip().upper()
-print(f"A letra A aparece {frs.count("A")} vezes na frase")
-print(f"A primeira letra A apareceu na {frs.find("A")+1} posição ")
-print(f"A ultima letra A aparece na posição de número: {frs.rfind("A")+1}")
