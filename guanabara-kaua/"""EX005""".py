31667f9a3eb88b46316a0b1cde6677606b8bfda4
Kaua-ceb <Kauapereirarangel1207@gmail.com>
2025-10-07 22:22:49 -0300

    Update and rename """EX005""".py to EX005.py

--- guanabara-kaua/"""EX005""".py
+++
@@ -1,6 +0,0 @@
-"""EX005"""
-
-num= int(input("digite um número: "))
-antecessor = num - 1
-sucessor = num + 1
-print(f"analisando o número {num}, seu antecessor é {antecessor} e o sucessor é {sucessor}")
---
+++ guanabara-kaua/EX005.py
@@ -0,0 +1,6 @@
+"""EX005"""
+
+num= int(input("digite um número: "))
+antecessor = num - 1
+sucessor = num + 1
+print(f"analisando o número {num}, seu antecessor é {antecessor} e o sucessor é {sucessor}")
