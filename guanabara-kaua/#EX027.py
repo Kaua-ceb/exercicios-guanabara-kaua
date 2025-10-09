cedcf0bb6624aed5be48767ab710392593b30c1e
Kaua-ceb <Kauapereirarangel1207@gmail.com>
2025-10-08 21:30:16 -0300

    Add files via upload

---
+++ guanabara-kaua/#EX027.py
@@ -0,0 +1,6 @@
+#EX027
+#ana e souza
+nome = str(input("Digite seu nome: "))
+print("Muito prazer em te conhecer! ")
+print(f"Seu primeiro nome é {nome[0:3]}")
+print(f"Seu ultimo nome é {nome[3:]}")
