89fd1b301c8c1ab51009a0ee7b6e3c6966a4e30a
Kaua-ceb <Kauapereirarangel1207@gmail.com>
2025-10-08 22:28:30 -0300

    Delete #EX029.py

--- #EX029.py
+++
@@ -1,8 +0,0 @@
-#EX029
-veloc = int(input("qual a velocidade do carro? "))
-if veloc >= 80:
-    print("Você foi multado")
-    multa = (veloc-80) *7
-    print(f"você vai ter que pagar R${multa}")
-else:
-  print("Você está dentro do limite de velocidade! Parábens!")
